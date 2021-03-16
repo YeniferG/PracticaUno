9a84f6691276398fdbf144bc92f66bc8880bd64d
Yenifer <guajeyenifer@gmail.com>
2021-03-15 22:37:24 -0500

    Carga Inicial

---
+++ src/App2.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola<code> Adsi</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yenifer Patricia Guaje Niño
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
